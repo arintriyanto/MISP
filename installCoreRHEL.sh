44dbfdbdc40044a48c5e55d2d912fa4eae18f13f
Arin Triyanto <arintriyanto@gmail.com>
2020-10-30 02:17:20 +0700

    no message

--- installCoreRHEL.sh
+++ installCoreRHEL.sh
@@ -1,3 +1,57 @@
+#!/bin/bash
+
+# Main Install on RHEL function
+installMISPRHEL () {
+
+    debug "Installing System Dependencies"
+    yumInstallCoreDeps
+
+    debug "Enabling Haveged for additional entropy"
+    sudo yum install haveged -y
+    sudo systemctl enable --now haveged.service
+
+    debug "Installing MISP code"
+    installCoreRHEL
+    
+    debug "Install Cake PHP"
+    installCake_RHEL
+    
+    echo "Core Intallation finished, check on port 443 to see the Web UI"
+  fi
+}
+# End installMISPRHEL ()
+
+yumInstallCoreDeps () {
+  # Install the dependencies:
+  sudo yum install gcc git zip rh-git218 \
+                   httpd24 \
+                   mod_ssl \
+                   rh-redis32 \
+                   rh-mariadb102 \
+                   libxslt-devel zlib-devel ssdeep-devel -y
+
+  # Enable and start redis
+  sudo systemctl enable --now rh-redis32-redis.service
+
+  WWW_USER="apache"
+  SUDO_WWW="sudo -H -u $WWW_USER"
+  RUN_PHP="/usr/bin/scl enable rh-php72"
+  PHP_INI="/etc/opt/rh/rh-php72/php.ini"
+  # Install PHP 7.2 from SCL, see https://www.softwarecollections.org/en/scls/rhscl/rh-php72/
+  sudo yum install rh-php72 rh-php72-php-fpm rh-php72-php-devel \
+                   rh-php72-php-mysqlnd \
+                   rh-php72-php-mbstring \
+                   rh-php72-php-xml \
+                   rh-php72-php-bcmath \
+                   rh-php72-php-opcache \
+                   rh-php72-php-gd -y
+
+  # Python 3.6 is now available in RHEL 7.7 base
+  sudo yum install python3 python3-devel -y
+
+  sudo systemctl enable --now rh-php72-php-fpm.service
+}
+
 # <snippet-begin 1_mispCoreInstall_RHEL.sh>
 installCoreRHEL () {
   # Download MISP using git in the /var/www/ directory.
@@ -114,3 +168,66 @@ installCoreRHEL () {
   sudo systemctl restart rh-php72-php-fpm.service
 }
 # <snippet-end 1_mispCoreInstall_RHEL.sh>
+
+
+installCake_RHEL ()
+{
+  sudo chown -R $WWW_USER:$WWW_USER $PATH_TO_MISP
+  sudo mkdir /usr/share/httpd/.composer
+  sudo chown $WWW_USER:$WWW_USER /usr/share/httpd/.composer
+  cd $PATH_TO_MISP/app
+  # Update composer.phar (optional)
+  #$SUDO_WWW $RUN_PHP -- php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+  #$SUDO_WWW $RUN_PHP -- php -r "if (hash_file('SHA384', 'composer-setup.php') === '48e3236262b34d30969dca3c37281b3b4bbe3221bda826ac6a9a62d6444cdb0dcd0615698a5cbe587c3f0fe57a54d8f5') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+  #$SUDO_WWW $RUN_PHP "php composer-setup.php"
+  #$SUDO_WWW $RUN_PHP -- php -r "unlink('composer-setup.php');"
+  $SUDO_WWW $RUN_PHP "php composer.phar require kamisama/cake-resque:4.1.2"
+  $SUDO_WWW $RUN_PHP "php composer.phar config vendor-dir Vendor"
+  $SUDO_WWW $RUN_PHP "php composer.phar install"
+
+  ## sudo yum install php-redis -y
+  sudo scl enable rh-php72 'pecl channel-update pecl.php.net'
+  sudo scl enable rh-php72 'yes no|pecl install redis'
+  echo "extension=redis.so" |sudo tee /etc/opt/rh/rh-php72/php-fpm.d/redis.ini
+  sudo ln -s /etc/opt/rh/rh-php72/php-fpm.d/redis.ini /etc/opt/rh/rh-php72/php.d/99-redis.ini
+  
+  sudo ln -s /usr/lib64/libfuzzy.so /usr/lib/libfuzzy.so
+  sudo scl enable rh-php72 'pecl install ssdeep'
+  echo "extension=ssdeep.so" |sudo tee /etc/opt/rh/rh-php72/php.d/99-ssdeep.ini
+
+  # Install gnupg extension
+  sudo yum install gpgme-devel -y
+  sudo scl enable rh-php72 'pecl install gnupg'
+  echo "extension=gnupg.so" |sudo tee /etc/opt/rh/rh-php72/php-fpm.d/gnupg.ini
+  sudo ln -s /etc/opt/rh/rh-php72/php-fpm.d/gnupg.ini /etc/opt/rh/rh-php72/php.d/99-gnupg.ini
+  sudo systemctl restart rh-php72-php-fpm.service
+
+  # If you have not yet set a timezone in php.ini
+  echo 'date.timezone = "Asia/Jakarta"' |sudo tee /etc/opt/rh/rh-php72/php-fpm.d/timezone.ini
+  sudo ln -s ../php-fpm.d/timezone.ini /etc/opt/rh/rh-php72/php.d/99-timezone.ini
+
+
+  # Recommended: Change some PHP settings in /etc/opt/rh/rh-php72/php.ini
+  # max_execution_time = 300
+  # memory_limit = 512M
+  # upload_max_filesize = 50M
+  # post_max_size = 50M
+  for key in upload_max_filesize post_max_size max_execution_time max_input_time memory_limit
+  do
+      sudo sed -i "s/^\($key\).*/\1 = $(eval echo \${$key})/" $PHP_INI
+  done
+  sudo systemctl restart rh-php72-php-fpm.service
+
+  # To use the scheduler worker for scheduled tasks, do the following:
+  sudo cp -fa $PATH_TO_MISP/INSTALL/setup/config.php $PATH_TO_MISP/app/Plugin/CakeResque/Config/config.php
+}
+# <snippet-begin 1_installCake_RHEL.sh>
+
+
+
+# If RHEL/CentOS is detected, run appropriate script
+if [[ "${FLAVOUR}" == "rhel" ]] || [[ "${FLAVOUR}" == "centos" ]]; then
+  installMISPRHEL
+  echo "Installation done !"
+  exit
+fi
